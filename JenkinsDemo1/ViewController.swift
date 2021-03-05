e8f4470f2b689743fb999f58da92636c11ca4928
kirvan520 <kiran.vangapalli@paysafe.com>
2021-03-05 13:26:37 +0100

    First Commit
    Initial commit of xcode project

---
+++ JenkinsDemo1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JenkinsDemo1
+//
+//  Created by Kiran Vangapalli on 05.03.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
